ce0aec3cdb02d9af6acb8053591e502125da793b
MOVITECHNOLOGY <miguelangel1620111@gmail.com>
2026-02-06 12:40:28 -0600

    Add files via upload

---
+++ appmovi.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+# Configuración de MOVI
+st.set_page_config(page_title="MOVI - Validador", page_icon="📦")
+st.title("📦 MOVI: Control de Devoluciones")
+
+# --- ESTA ES LA PARTE IMPORTANTE ---
+# Borra TU_TOKEN_AQUI (pero deja las comillas) y pega tu Token de BoxHero
+TOKEN = "7af32261-1ee8-4d53-b1b5-77afb233d446" 
+# ----------------------------------
+
+HEADERS = {"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}
+BASE_URL = "https://api.boxhero.io/v1"
+
+# Interfaz del empleado
+nro_factura = st.text_input("1. Ingrese o escanee el Folio de Factura")
+
+if nro_factura:
+    st.info(f"Conectado a BoxHero. Buscando datos de: {nro_factura}")
+    
+    scanned_code = st.text_input("2. Escanee el Código de Barras del producto")
+    
+    if scanned_code:
+        # Aquí es donde el programa hace la magia de comparar cantidades
+        st.warning(f"Validando {scanned_code}... Espere un momento.")
+        # El resto del código de validación lo agregaremos cuando veas que este abre bien.
+
+
+
